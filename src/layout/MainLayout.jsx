1a9bab205ce9ec31dffd8b0d4ec710504963ea3e
Valeria Gabrielli <gabvale53@gmail.com>
2026-02-27 18:18:15 +0100

    feat: aggiunto react-bootstrap

--- src/layout/MainLayout.jsx
+++ src/layout/MainLayout.jsx
@@ -1,12 +1,17 @@
 import { Outlet } from "react-router-dom";
-
+import { Link } from "react-router-dom";
 function MainLayout() {
   return (
     <>
-     <nav className="navbar bg-body-tertiary mb-4 d-flex justify-content-center">
-      <h1>Documentello di Viaggello</h1>
+     <nav className="navbar bg-body-tertiary mb-4 d-flex justify-content-space-between">
+      <div className="container">
+        <h1>Viaggello</h1>
+        
+      </div>
+
      </nav>
       <main>
+        
         <Outlet />
       </main>
     </>
