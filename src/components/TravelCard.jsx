285223c87898a26ecdac82ec153f8965868651e7
marcotristani <marco.tristani@hotmail.it>
2026-02-27 17:27:43 +0100

    flat:modifica travel card

--- src/components/TravelCard.jsx
+++ src/components/TravelCard.jsx
@@ -2,6 +2,12 @@ function TravelCard() {
   return (
     <div>
       <h1>nome cognome</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat optio
+        ipsam, dolor quia, excepturi necessitatibus corrupti dicta doloribus
+        itaque pariatur minima, veniam animi recusandae rerum esse hic! Error,
+        sit. Inventore!
+      </p>
     </div>
   );
 }
