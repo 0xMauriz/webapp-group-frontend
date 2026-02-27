fa29c33046fe9f016adb2284dd146885b0f370af
Valeria Gabrielli <gabvale53@gmail.com>
2026-02-27 17:08:08 +0100

    test approve changes

--- src/App.jsx
+++ src/App.jsx
@@ -8,11 +8,6 @@ import Homepage from "./pages/Homepage";
 import TravelDetail from "./pages/TravelDetail";
 
 function App() {
-<<<<<<< HEAD
-
-
-=======
->>>>>>> 311f7785092c1f0378da6ff6248c51d3f5ad8aa5
   return (
     <BrowserRouter>
       <Routes>
