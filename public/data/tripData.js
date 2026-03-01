695d25f02eeff5688a3a28368b0ea50dd43f8c82
Maurizio Piroddi <x.hacker.hiphop99@gmail.com>
2026-03-01 01:19:58 +0100

    Array per i dati dei viaggi

--- public/data/tripData.js
+++ public/data/tripData.js
@@ -1,24 +1,167 @@
 const arrayViaggi = [
 
     {
+        id: 1,
         tripName: "Trip to Rome",
         destination: "Rome",
-        tripDuration: "1h 40min",
+        tripDuration: "1h 30min",
         tripStart: "11:30",
         tripEnd: "13:00",
         travelerData: [
-            {},
-            {},
-            {}
+            {
+                id: 1,
+                trip_id: 1,
+                name: "Mario",
+                surname: "Rossi",
+                dateOfBirth: "1984-02-17",
+                age: "42",
+                fiscalCode: "MRRSS84B17F808K",
+                placeOfBirth: "Cinisello Balsamo",
+                nationality: "ITA"
+            },
+            {
+                id: 2,
+                trip_id: 1,
+                name: "Frank",
+                surname: "Shire",
+                dateOfBirth: "1974-03-07",
+                age: "52",
+                fiscalCode: "FRNCBRN74B07F809Z",
+                placeOfBirth: "Birmingham",
+                nationality: "ENG"
+            },
+            {
+                id: 3,
+                trip_id: 1,
+                name: "Pablo",
+                surname: "Gutierres",
+                dateOfBirth: "1995-01-27",
+                age: "31",
+                fiscalCode: "JNGTRRS95B27F800K",
+                placeOfBirth: "Guadalajara",
+                nationality: "MEX"
+            },
+            {
+                id: 4,
+                trip_id: 1,
+                name: "Paolo",
+                surname: "Ferri",
+                dateOfBirth: "1993-10-11",
+                age: "31",
+                fiscalCode: "PLFRR93B11F700K",
+                placeOfBirth: "Muravera",
+                nationality: "ITA"
+            },
+            {
+                id: 5,
+                trip_id: 1,
+                name: "Eleni",
+                surname: "Papadopoulos",
+                dateOfBirth: "1986-01-02",
+                age: "40",
+                fiscalCode: "LNPPDPLS86B02F800K",
+                placeOfBirth: "Attica",
+                nationality: "GRC"
+            }
         ]
     },
     {
-
+        id: 2,
+        tripName: "Trip to Zurich",
+        destination: "Zurich",
+        tripDuration: "3h 40min",
+        tripStart: "13:30",
+        tripEnd: "17:10",
+        travelerData: [
+            {
+                id: 1,
+                trip_id: 2,
+                name: "Katy",
+                surname: "Smith",
+                dateOfBirth: "1980-08-09",
+                age: "46",
+                fiscalCode: "KTSMT80B09F609Z",
+                placeOfBirth: "Texas",
+                nationality: "USA"
+            },
+            {
+                id: 2,
+                trip_id: 2,
+                name: "Francesco",
+                surname: "Verdi",
+                dateOfBirth: "1970-06-07",
+                age: "56",
+                fiscalCode: "FRNCSCVRD70B07F609Z",
+                placeOfBirth: "Milano",
+                nationality: "ITA"
+            },
+            {
+                id: 3,
+                trip_id: 2,
+                name: "Haruto",
+                surname: "Tanaka",
+                dateOfBirth: "2002-05-27",
+                age: "24",
+                fiscalCode: "RTTNK02B27F800K",
+                placeOfBirth: "Tokyo",
+                nationality: "JPN"
+            }
+        ]
     },
     {
-
+        id: 3,
+        tripName: "Trip to Paris",
+        destination: "Paris",
+        tripDuration: "7h 00min",
+        tripStart: "9:00",
+        tripEnd: "16:00",
+        travelerData: [
+            {
+                id: 1,
+                trip_id: 3,
+                name: "Anna",
+                surname: "Müller",
+                dateOfBirth: "1983-11-02",
+                age: "43",
+                fiscalCode: "NMLR83B02F609Z",
+                placeOfBirth: "Bavaria",
+                nationality: "DEU"
+            },
+            {
+                id: 2,
+                trip_id: 3,
+                name: "Lukas",
+                surname: "Gruber",
+                dateOfBirth: "1997-06-22",
+                age: "39",
+                fiscalCode: "LKSGRBR97B22F609Z",
+                placeOfBirth: "Vienna",
+                nationality: "AUT"
+            },
+            {
+                id: 3,
+                trip_id: 3,
+                name: "Giovanni",
+                surname: "Perrito",
+                dateOfBirth: "2001-01-23",
+                age: "25",
+                fiscalCode: "GVNNPRRT01B23F810A",
+                placeOfBirth: "Ancona",
+                nationality: "ITA"
+            },
+            {
+                id: 4,
+                trip_id: 3,
+                name: "Aino",
+                surname: "Virtanen",
+                dateOfBirth: "1989-03-18",
+                age: "24",
+                fiscalCode: "NVRTNN89B18F821K",
+                placeOfBirth: "Uusimaa",
+                nationality: "FIN"
+            }
+        ]
     }
-
 ];
 
 export default arrayViaggi
