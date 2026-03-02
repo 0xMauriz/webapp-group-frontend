3f25c7a06136bab1873d5ac45765a0e22f4a22b5
Niccolo Rojas <niccolo.rojas.ge@gmail.com>
2026-03-02 11:11:04 +0100

    Add card Homepage

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -3,10 +3,31 @@ function Homepage() {
     <>
       <h1>prova homepage</h1>
       <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor nulla
-        dignissimos ipsa tempora ipsum, consequatur sit facere aliquam
-        accusantium, corporis iste ipsam autem libero. Dicta quos error
-        accusantium quisquam! Dolore!
+        <div className="card" style={{ width: "18rem" }}>
+        <img
+          src="https://via.placeholder.com/286x180"
+          className="card-img-top"
+          alt="Immagine esempio"
+        />
+
+        <div className="card-body">
+          <h5 className="card-title">Titolo Card</h5>
+          <p className="card-text">
+            Un breve testo di esempio per riempire il contenuto della card.
+          </p>
+        </div>
+
+        <ul className="list-group list-group-flush">
+          <li className="list-group-item">Primo elemento</li>
+          <li className="list-group-item">Secondo elemento</li>
+          <li className="list-group-item">Terzo elemento</li>
+        </ul>
+
+        <div className="card-body">
+          <a href="/" className="card-link">Link 1</a>
+          <a href="/" className="card-link">Link 2</a>
+        </div>
+      </div>
       </p>
     </>
   );
