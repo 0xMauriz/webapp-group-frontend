0bee71f6d4ef29bca5fec6add6b8f8051ce763bc
marcotristani <marco.tristani@hotmail.it>
2026-03-02 13:40:35 +0100

    sistemato errore

--- src/components/TravelCard.jsx
+++ src/components/TravelCard.jsx
@@ -1,13 +1,9 @@
-function TravelCard() {
-  tripName,
-    destination,
-    tripDuration,
-    tripStart,
-    tripEnd
+function TravelCard(props) {
+  const { tripName, destination, tripDuration, tripStart, tripEnd } = props;
   return (
     <div>
       <p>
-        <div key={trip.id} className="card" style={{ width: "18rem" }}>
+        <div className="card" style={{ width: "18rem" }}>
           <img
             src="https://via.placeholder.com/286x180"
             className="card-img-top"
@@ -17,16 +13,19 @@ function TravelCard() {
           <div className="card-body">
             <h5 className="card-title">{tripName}</h5>
             <p className="card-text">
-              <strong>destination:</strong>{destination}
+              <strong>destination:</strong>
+              {destination}
             </p>
           </div>
 
           <ul className="list-group list-group-flush">
             <p className="card-text">
-              <strong>duration:</strong>{tripDuration}
+              <strong>duration:</strong>
+              {tripDuration}
             </p>
             <p className="card-text">
-              <strong>Dates:</strong>{tripStart} - {tripEnd}
+              <strong>Dates:</strong>
+              {tripStart} - {tripEnd}
             </p>
           </ul>
         </div>
