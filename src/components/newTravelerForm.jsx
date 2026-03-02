28407c10c6c6569f6e74cb66a4a32e3beff9a734
Maurizio Piroddi <x.hacker.hiphop99@gmail.com>
2026-03-02 13:21:35 +0100

    Cambio del raggruppamento nei file homepage e card

--- src/components/newTravelerForm.jsx
+++ src/components/newTravelerForm.jsx
@@ -1,22 +1,49 @@
+import { useState } from "react"
+
+
 function newTravelerForm() {
+
+    const [travelers, setTravelers] = useState([
+        trip_id = "",
+
+    ]);
+
+
 
     return (
         <><form>
             <div className="mb-3">
-                <label for="exampleInputEmail1" className="form-label">Email address</label>
-                <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
+                <label htmlFor="trip-id" className="form-label">Id a cui il viaggio è associato: </label>
+                <input type="text" className="form-control" id="trip-id" aria-describedby="emailHelp" />
                 <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
             </div>
             <div className="mb-3">
-                <label for="exampleInputPassword1" className="form-label">Password</label>
-                <input type="password" className="form-control" id="exampleInputPassword1" />
+                <label htmlFor="exampleInputPassword1" className="form-label">Password</label>
+                <input type="text" className="form-control" id="exampleInputPassword1" />
             </div>
             <div className="mb-3 form-check">
                 <input type="checkbox" className="form-check-input" id="exampleCheck1" />
                 <label className="form-check-label" for="exampleCheck1">Check me out</label>
+            </div><div className="mb-3">
+                <label htmlFor="exampleInputEmail1" className="form-label">Email address</label>
+                <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
+                <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
+            </div><div className="mb-3">
+                <label htmlFor="exampleInputEmail1" className="form-label">Email address</label>
+                <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
+                <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
             </div>
-            <button type="submit" className="btn btn-primary">Submit</button>
+            <button type="submit" className="btn btn-primary my-3">Submit</button>
         </form>
+
+            id: 1,
+            trip_id: 1,
+            firstName: "John",
+            lastName: "Smith",
+            taxCode: "USASMTH90A12X123Y",
+            email: "john.smith@email.com",
+            phone: "+1 202 555 0101",
+            emergencyContact:
 
         </>
     )
