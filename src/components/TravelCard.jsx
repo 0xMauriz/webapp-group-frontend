6bda53830a5dfdcdd5088f3b5e2236cb03547611
marcotristani <marco.tristani@hotmail.it>
2026-03-02 13:43:20 +0100

    sistemato altro errore

--- src/components/TravelCard.jsx
+++ src/components/TravelCard.jsx
@@ -2,34 +2,32 @@ function TravelCard(props) {
   const { tripName, destination, tripDuration, tripStart, tripEnd } = props;
   return (
     <div>
-      <p>
-        <div className="card" style={{ width: "18rem" }}>
-          <img
-            src="https://via.placeholder.com/286x180"
-            className="card-img-top"
-            alt="Immagine esempio"
-          />
+      <div className="card" style={{ width: "18rem" }}>
+        <img
+          src="https://via.placeholder.com/286x180"
+          className="card-img-top"
+          alt="Immagine esempio"
+        />
 
-          <div className="card-body">
-            <h5 className="card-title">{tripName}</h5>
-            <p className="card-text">
-              <strong>destination:</strong>
-              {destination}
-            </p>
-          </div>
-
-          <ul className="list-group list-group-flush">
-            <p className="card-text">
-              <strong>duration:</strong>
-              {tripDuration}
-            </p>
-            <p className="card-text">
-              <strong>Dates:</strong>
-              {tripStart} - {tripEnd}
-            </p>
-          </ul>
+        <div className="card-body">
+          <h5 className="card-title">{tripName}</h5>
+          <p className="card-text">
+            <strong>destination:</strong>
+            {destination}
+          </p>
         </div>
-      </p>
+
+        <ul className="list-group list-group-flush">
+          <p className="card-text">
+            <strong>duration:</strong>
+            {tripDuration}
+          </p>
+          <p className="card-text">
+            <strong>Dates:</strong>
+            {tripStart} - {tripEnd}
+          </p>
+        </ul>
+      </div>
     </div>
   );
 }
