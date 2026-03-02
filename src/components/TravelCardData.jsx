b17c2e03371bcdb4c3fe834656322f064598f0dd
marcotristani <marco.tristani@hotmail.it>
2026-03-02 15:11:54 +0100

    link a card homepage

--- src/components/TravelCardData.jsx
+++ src/components/TravelCardData.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { Link } from "react-router-dom";
 import arrayViaggi from "../../src/data/tripData.js";
 import TravelCard from "./TravelCard.jsx";
 
@@ -6,18 +7,22 @@ function TravelCardData() {
   const [trips, setTrips] = useState(arrayViaggi);
 
   return (
-    <>
-      {trips.map((trip) => (
-        <TravelCard
-          key={trip.id}
-          tripName={trip.tripName}
-          destination={trip.destination}
-          tripDuration={trip.tripDuration}
-          tripStart={trip.tripStart}
-          tripEnd={trip.tripEnd}
-        />
-      ))}
-    </>
+    <div className="row">
+      <div className="col">
+        {trips.map((trip) => (
+          <Link to={`/travel/${trip.id}`}>
+            <TravelCard
+              key={trip.id}
+              tripName={trip.tripName}
+              destination={trip.destination}
+              tripDuration={trip.tripDuration}
+              tripStart={trip.tripStart}
+              tripEnd={trip.tripEnd}
+            />
+          </Link>
+        ))}
+      </div>
+    </div>
   );
 }
 
