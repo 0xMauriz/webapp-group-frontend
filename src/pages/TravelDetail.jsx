acbb9dc516b17cde8e743b5961dd2652019960b8
marcotristani <marco.tristani@hotmail.it>
2026-03-03 13:15:24 +0100

    class al for

--- src/pages/TravelDetail.jsx
+++ src/pages/TravelDetail.jsx
@@ -8,7 +8,6 @@ import SearchBar from "../components/SearchBar";
 import arrayViaggi from "./../data/tripData";
 
 function TravelDetail() {
-
   // Logica per permettere l'aggiunta di nuovi viaggiatori nell'array e il rendering in pagina
 
   const [travelersList, setTravelersList] = useState(arrayViaggi);
@@ -22,26 +21,25 @@ function TravelDetail() {
     phone: "",
     emergencyContact: {
       name: "",
-      phone: ""
-    }
-
+      phone: "",
+    },
   });
 
   function handleChange(e) {
     const { name, value } = e.target;
 
     if (name === "emergencyName" || name === "emergencyPhone") {
-      setTraveler(prev => ({
+      setTraveler((prev) => ({
         ...prev,
         emergencyContact: {
           ...prev.emergencyContact,
-          [name === "emergencyName" ? "name" : "phone"]: value
-        }
+          [name === "emergencyName" ? "name" : "phone"]: value,
+        },
       }));
     } else {
-      setTraveler(prev => ({
+      setTraveler((prev) => ({
         ...prev,
-        [name]: value
+        [name]: value,
       }));
     }
   }
@@ -49,15 +47,15 @@ function TravelDetail() {
   function handleSubmit(e) {
     e.preventDefault();
 
-    setTravelersList(prevList =>
-      prevList.map(travel =>
+    setTravelersList((prevList) =>
+      prevList.map((travel) =>
         travel.id == travel_id
           ? {
-            ...travel,
-            travelerData: [...travel.travelerData, traveler]
-          }
-          : travel
-      )
+              ...travel,
+              travelerData: [...travel.travelerData, traveler],
+            }
+          : travel,
+      ),
     );
 
     setTraveler({
@@ -69,19 +67,14 @@ function TravelDetail() {
       phone: "",
       emergencyContact: {
         name: "",
-        phone: ""
-      }
+        phone: "",
+      },
     });
   }
 
-
   console.log(travelersList);
 
-
-
-
   //recuper id del viaggio da url
-
 
   const { travel_id } = useParams();
 
@@ -124,7 +117,11 @@ function TravelDetail() {
             </button>
           </Link>
           <h1>Viaggiatori:</h1>
-          <SearchBar search={searchTraveler} setSearch={setSearchTraveler} className />
+          <SearchBar
+            search={searchTraveler}
+            setSearch={setSearchTraveler}
+            className
+          />
           <ul className="list-group list-group-flush">
             {searched_travelers.map((traveler) => (
               <TravelerCards key={traveler.id} traveler={traveler} />
@@ -133,44 +130,125 @@ function TravelDetail() {
         </div>
         <div className="container  d-none d-lg-block">
           <div className="card m-5">
-            <form onSubmit={handleSubmit}>
+            <form
+              onSubmit={handleSubmit}
+              className="p-5 bg-success text-light rounded text-center"
+            >
               <div className="mb-3">
-                <label htmlFor="trip-id" className="form-label">Immettere id del viaggio a cui il viaggiatore è associato: </label>
-                <input name="trip_id" type="text" className="form-control" id="trip-id" aria-describedby="emailHelp" value={traveler.trip_id} onChange={handleChange} />
+                <label htmlFor="trip-id" className="form-label">
+                  Immettere id del viaggio a cui il viaggiatore è
+                  associato:{" "}
+                </label>
+                <input
+                  name="trip_id"
+                  type="text"
+                  className="form-control"
+                  id="trip-id"
+                  aria-describedby="emailHelp"
+                  value={traveler.trip_id}
+                  onChange={handleChange}
+                />
               </div>
               <div className="mb-3">
-                <label htmlFor="nome-viaggiatore" className="form-label">Immettere nome del viaggiatore: </label>
-                <input name="firstName" type="text" className="form-control" id="traveler-name" value={traveler.firstName} onChange={handleChange} />
+                <label htmlFor="nome-viaggiatore" className="form-label">
+                  Immettere nome del viaggiatore:{" "}
+                </label>
+                <input
+                  name="firstName"
+                  type="text"
+                  className="form-control"
+                  id="traveler-name"
+                  value={traveler.firstName}
+                  onChange={handleChange}
+                />
               </div>
               <div className="mb-3">
-                <label className="form-label" htmlFor="traveler-surname">Immettere cognome del viaggiatore: </label>
-                <input name="lastName" type="text" className="form-control" id="traveler-surname" value={traveler.lastName} onChange={handleChange} />
+                <label className="form-label" htmlFor="traveler-surname">
+                  Immettere cognome del viaggiatore:{" "}
+                </label>
+                <input
+                  name="lastName"
+                  type="text"
+                  className="form-control"
+                  id="traveler-surname"
+                  value={traveler.lastName}
+                  onChange={handleChange}
+                />
               </div>
               <div className="mb-3">
-                <label htmlFor="tax-code" className="form-label">Immettere codice fiscale del viaggiatore: </label>
-                <input name="taxCode" type="text" className="form-control" id="tax-code" value={traveler.taxCode} onChange={handleChange} />
+                <label htmlFor="tax-code" className="form-label">
+                  Immettere codice fiscale del viaggiatore:{" "}
+                </label>
+                <input
+                  name="taxCode"
+                  type="text"
+                  className="form-control"
+                  id="tax-code"
+                  value={traveler.taxCode}
+                  onChange={handleChange}
+                />
               </div>
               <div className="mb-3">
-                <label htmlFor="traveler-email" className="form-label">Immettere indirizzo Mail: </label>
-                <input name="email" type="email" className="form-control" id="traveler-email" aria-describedby="emailHelp" value={traveler.email} onChange={handleChange} />
+                <label htmlFor="traveler-email" className="form-label">
+                  Immettere indirizzo Mail:{" "}
+                </label>
+                <input
+                  name="email"
+                  type="email"
+                  className="form-control"
+                  id="traveler-email"
+                  aria-describedby="emailHelp"
+                  value={traveler.email}
+                  onChange={handleChange}
+                />
               </div>
               <div className="mb-3">
-                <label htmlFor="traveler-phone" className="form-label">Immettere numero di telefono del viaggiatore: </label>
-                <input name="phone" type="text" className="form-control" id="traveler-phone" value={traveler.phone} onChange={handleChange} />
+                <label htmlFor="traveler-phone" className="form-label">
+                  Immettere numero di telefono del viaggiatore:{" "}
+                </label>
+                <input
+                  name="phone"
+                  type="text"
+                  className="form-control"
+                  id="traveler-phone"
+                  value={traveler.phone}
+                  onChange={handleChange}
+                />
               </div>
               <div className="mb-3">
-                <label htmlFor="emergency-name" className="form-label">Immettere nome proprietario del numero di telefono di emergenza del viaggiatore: </label>
-                <input name="emergencyName" type="text" className="form-control" id="emergency-name" value={traveler.emergencyContact.name} onChange={handleChange} />
+                <label htmlFor="emergency-name" className="form-label">
+                  Immettere nome proprietario del numero di telefono di
+                  emergenza del viaggiatore:{" "}
+                </label>
+                <input
+                  name="emergencyName"
+                  type="text"
+                  className="form-control"
+                  id="emergency-name"
+                  value={traveler.emergencyContact.name}
+                  onChange={handleChange}
+                />
               </div>
               <div className="mb-3">
-                <label htmlFor="emergency-number" className="form-label">Immettere numero di telefono del proprietario del numero di telefono di emergenza del viaggiatore: </label>
-                <input name="emergencyPhone" type="text" className="form-control" id="emergency-number" value={traveler.emergencyContact.phone} onChange={handleChange} />
+                <label htmlFor="emergency-number" className="form-label">
+                  Immettere numero di telefono del proprietario del numero di
+                  telefono di emergenza del viaggiatore:{" "}
+                </label>
+                <input
+                  name="emergencyPhone"
+                  type="text"
+                  className="form-control"
+                  id="emergency-number"
+                  value={traveler.emergencyContact.phone}
+                  onChange={handleChange}
+                />
               </div>
-              <button type="submit" className="btn btn-primary my-3">Submit</button>
+              <button type="submit" className="btn btn-outline-light my-3">
+                Submit
+              </button>
             </form>
           </div>
         </div>
-
       </div>
     </div>
   );
